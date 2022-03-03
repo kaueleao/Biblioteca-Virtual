21935a7a32ad51b0b400c19b7623c808f2dde507
kaueleao <62720673+kaueleao@users.noreply.github.com>
2022-03-02 22:39:54 -0300

    criacao-rotas-e-create

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
+import { LivroCreateComponent } from './components/livro/livro-create/livro-create.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './views/home/home.component';
+import {LivrosCrudComponent} from './views/livros-crud/livros-crud.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{
+  path: "",
+  component: HomeComponent
+},{
+  path: "livros",
+  component: LivrosCrudComponent
+},
+{
+  path: "livros/create",
+  component: LivroCreateComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
