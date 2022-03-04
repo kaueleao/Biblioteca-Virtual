5b2ab860c479a9babeb42ae3d348d576bd93a654
kaueleao <62720673+kaueleao@users.noreply.github.com>
2022-03-03 21:54:10 -0300

    implementacao-read

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { HttpClientModule} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { LivroReadComponent } from './components/livro/livro-read/livro-read.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import { MatInputModule } from '@angular/material/input';
     NavComponent,
     HomeComponent,
     LivrosCrudComponent,
-    LivroCreateComponent
+    LivroCreateComponent,
+    LivroReadComponent
   ],
   imports: [
     BrowserModule,
