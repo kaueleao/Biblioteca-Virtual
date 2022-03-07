b56afc20d311bc4c88c7d6d44b01c342e60093ef
kaueleao <kaue44@yahoo.com.br>
2022-03-06 21:57:52 -0300

    implementado-rota-componente-update

---
+++ frontend/src/app/components/livro/livro-update/livro-update.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-livro-update',
+  templateUrl: './livro-update.component.html',
+  styleUrls: ['./livro-update.component.css']
+})
+export class LivroUpdateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
