14ec94bfa943455937bfef13d62856db158db40a
kaueleao <kaue44@yahoo.com.br>
2022-06-08 21:15:22 -0300

    autenticacao-simples

--- frontend/src/app/components/livro/livro-login/livro-login.component.ts
+++ frontend/src/app/components/livro/livro-login/livro-login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LivroService } from './../livro.service';
 
 @Component({
   selector: 'app-livro-login',
@@ -7,13 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LivroLoginComponent implements OnInit {
 
-  constructor() { }
+  usuario: string | undefined;
+  senha: string | undefined;
+
+  constructor(private router:Router, private LivroService: LivroService) { }
 
   ngOnInit(): void {
   }
 
+
   fazerLogin(){
     
+    if(this.usuario == "biblioteca" && this.senha == "admin123") // Usuário administrador
+    {
+      this.router.navigate(['/'])
+      this.LivroService.showMenssage('Login Efetuado!'); 
+    }
+    
+    else if(this.usuario == "aluno" && this.senha == "aluno") // Usuário alunos
+    {
+      this.router.navigate(['/livros/consulta'])
+      this.LivroService.showMenssage('Login Efetuado!');
+    }
+    
+    else{
+      this.LivroService.showMenssage('Usuário e/ou Senha Errados!');
+    }
+    
   }
+  
 
 }
